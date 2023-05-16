615c2a8e0f525e4cc560973a51a2dc1f7e2ddd5a
riyaansh <dhokerahuls@gmail.com>
2023-05-16 16:12:04 +0530

    this is first commit

---
+++ src/test/java/DemoPackage1/Class1.java
@@ -0,0 +1,18 @@
+package DemoPackage1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void Demo1() {
+		System.out.println("demo 1");
+	}
+	
+	public static void Demo2() {
+		System.out.println("demo 1");
+	}
+
+}
---
+++ src/test/java/DemoPackage1/Class3.java
@@ -0,0 +1,10 @@
+package DemoPackage1;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
