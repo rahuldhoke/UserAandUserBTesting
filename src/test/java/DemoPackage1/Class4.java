4b7e20598c5d31bb10bd71e1d51031c6fba9446a
riyaansh <dhokerahuls@gmail.com>
2023-05-16 16:41:33 +0530

    classes added

---
+++ src/test/java/DemoPackage1/Class4.java
@@ -0,0 +1,13 @@
+package DemoPackage1;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+
+
+
+	}
+	public static void codeupdted() {
+		System.out.println("codeupdted 1");
+	}
+}
